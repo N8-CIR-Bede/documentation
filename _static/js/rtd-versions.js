6a013fce9dcb479c94f537124a559e20c5d08021
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-10 19:36:12 +0000

    Use MutationObserver to wait for rst-vurrent-verison to appear

--- _static/js/rtd-versions.js
+++ _static/js/rtd-versions.js
@@ -1,9 +1,24 @@
-$( document ).ready(function() {
-  $('.rst-current-version .fa-caret-down').toggleClass(['fa-caret-up', 'fa-caret-down'])
+// set up the mutation observer
+var observer = new MutationObserver(function (mutations, me) {
+  // `mutations` is an array of mutations that occurred
+  // `me` is the MutationObserver instance
+  var rst_current_version = $('.rst-current-version');
+  if (rst_current_version) {
+    $('.rst-current-version .fa-caret-down').toggleClass(['fa-caret-up', 'fa-caret-down'])
 
-  $('.rst-current-version').click(function() {
-    $('.rst-current-version .fa-caret-down, .rst-current-version .fa-caret-up')
-      .toggleClass(['fa-caret-up', 'fa-caret-down']);
-    $('.rst-other-versions').toggle();
-  });
+    $('.rst-current-version').click(function() {
+      $('.rst-current-version .fa-caret-down, .rst-current-version .fa-caret-up')
+        .toggleClass(['fa-caret-up', 'fa-caret-down']);
+      $('.rst-other-versions').toggle();
+    });
+
+    me.disconnect(); // stop observing
+    return;
+  }
+});
+
+// start observing
+observer.observe(document, {
+  childList: true,
+  subtree: true
 });
