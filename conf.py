848b97b34219f883b2d84b15c81e47ddaa97fde8
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-14 10:45:39 +0000

    Try adding a rtd-sidebar element to allow ads

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ import sphinx_bootstrap_theme
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -159,7 +159,7 @@ texinfo_documents = [
 ]
 
 
-html_sidebars = { '**': ['localtoc.html', 'relations.html'], }
+html_sidebars = { '**': ['localtoc.html', 'relations.html', 'sidebar-ads.html'], }
 
 def setup(app):
     on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
