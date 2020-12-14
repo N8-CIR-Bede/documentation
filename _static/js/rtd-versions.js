f9ec3989303f78188305fe1bde547cee32a0a033
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-14 08:51:56 +0000

    Revert "Try waiting for document ready before setting up observer"
    3cd4d0c5142613a36253a65373db9a0ea5e43899

--- _static/js/rtd-versions.js
+++ _static/js/rtd-versions.js
@@ -1,4 +1,7 @@
-var setupRstVersions = function() {
+// set up the mutation observer
+var observer = new MutationObserver(function (mutations, me) {
+  // `mutations` is an array of mutations that occurred
+  // `me` is the MutationObserver instance
   var rst_current_version = $('.rst-current-version');
   if (rst_current_version) {
     $('.rst-current-version .fa-caret-down').toggleClass(['fa-caret-up', 'fa-caret-down'])
@@ -8,30 +11,14 @@ var setupRstVersions = function() {
         .toggleClass(['fa-caret-up', 'fa-caret-down']);
       $('.rst-other-versions').toggle();
     });
-    return true;
-  } else {
-    return false;
-  }
-}
 
-// set up the mutation observer
-var observer = new MutationObserver(function (mutations, me) {
-  // `mutations` is an array of mutations that occurred
-  // `me` is the MutationObserver instance
-  var found = setupRstVersions();
-  if (found) {
     me.disconnect(); // stop observing
     return;
   }
 });
 
-$(document).ready(function() {
-  var found = setupRstVersions();
-  if (!found) {
-    // start observing
-    observer.observe(document, {
-      childList: true,
-      subtree: true
-    });
-  }
+// start observing
+observer.observe(document, {
+  childList: true,
+  subtree: true
 });
