3cd4d0c5142613a36253a65373db9a0ea5e43899
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-12-14 08:39:46 +0000

    Try waiting for document ready before setting up observer

--- _static/js/rtd-versions.js
+++ _static/js/rtd-versions.js
@@ -1,7 +1,4 @@
-// set up the mutation observer
-var observer = new MutationObserver(function (mutations, me) {
-  // `mutations` is an array of mutations that occurred
-  // `me` is the MutationObserver instance
+var setupRstVersions = function() {
   var rst_current_version = $('.rst-current-version');
   if (rst_current_version) {
     $('.rst-current-version .fa-caret-down').toggleClass(['fa-caret-up', 'fa-caret-down'])
@@ -11,14 +8,30 @@ var observer = new MutationObserver(function (mutations, me) {
         .toggleClass(['fa-caret-up', 'fa-caret-down']);
       $('.rst-other-versions').toggle();
     });
+    return true;
+  } else {
+    return false;
+  }
+}
 
+// set up the mutation observer
+var observer = new MutationObserver(function (mutations, me) {
+  // `mutations` is an array of mutations that occurred
+  // `me` is the MutationObserver instance
+  var found = setupRstVersions();
+  if (found) {
     me.disconnect(); // stop observing
     return;
   }
 });
 
-// start observing
-observer.observe(document, {
-  childList: true,
-  subtree: true
+$(document).ready(function() {
+  var found = setupRstVersions();
+  if (!found) {
+    // start observing
+    observer.observe(document, {
+      childList: true,
+      subtree: true
+    });
+  }
 });
